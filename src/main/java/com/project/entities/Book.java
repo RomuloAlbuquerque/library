bd321af13288e1e1e7719054d3cce113c9a7f197
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-11-25 22:20:31 -0300

    Object-Relational Mapping

--- src/main/java/com/project/entities/Book.java
+++ src/main/java/com/project/entities/Book.java
@@ -4,10 +4,22 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_books")
 public class Book implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String author;
 	private String title;
@@ -16,6 +28,8 @@ public class Book implements Serializable {
 	private Integer numberPages;
 	private String category;
 	private Date publicationDate;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant registrationInstant;
 	
 	public Book() {
@@ -103,6 +117,7 @@ public class Book implements Serializable {
 		return registrationInstant;
 	}
 
+	@PrePersist
 	public void prePersist() {
 		registrationInstant = Instant.now();
 	}
