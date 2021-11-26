a16c437c25d14d6d0913049bde9226a8286ddd5a
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-11-26 00:06:35 -0300

    Add @RequestMapping and @GetMapping annotations in the BookResource class and findById method respectively

--- src/main/java/com/project/resources/BookResource.java
+++ src/main/java/com/project/resources/BookResource.java
@@ -2,18 +2,22 @@ package com.project.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.dto.BookDTO;
 import com.project.services.BookService;
 
 @RestController
+@RequestMapping(value = "/books")
 public class BookResource {
 	
 	@Autowired
 	private BookService service;
 	
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<BookDTO> findById(@PathVariable Long id){
 		BookDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
