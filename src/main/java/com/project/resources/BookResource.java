8ffd32b6d4e8ed72132c83acb13a62e772013b01
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-11-25 23:58:01 -0300

    Created findById() method in the BookService and BookResource

--- src/main/java/com/project/resources/BookResource.java
+++ src/main/java/com/project/resources/BookResource.java
@@ -1,8 +1,11 @@
 package com.project.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.dto.BookDTO;
 import com.project.services.BookService;
 
 @RestController
@@ -10,5 +13,10 @@ public class BookResource {
 	
 	@Autowired
 	private BookService service;
+	
+	public ResponseEntity<BookDTO> findById(@PathVariable Long id){
+		BookDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
 
 }
--- src/main/java/com/project/services/BookService.java
+++ src/main/java/com/project/services/BookService.java
@@ -1,8 +1,13 @@
 package com.project.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.project.dto.BookDTO;
+import com.project.entities.Book;
 import com.project.repositories.BookRepository;
 
 @Service
@@ -10,5 +15,12 @@ public class BookService {
 	
 	@Autowired
 	private BookRepository repository;
+	
+	@Transactional
+	public BookDTO findById(Long id) {
+		Optional<Book> obj = repository.findById(id);
+		Book entity = obj.get();
+		return new BookDTO(entity);
+	}
 
 }
