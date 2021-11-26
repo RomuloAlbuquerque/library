b6e540524ca8d555efd169e787dc587582d32096
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-11-25 22:17:32 -0300

    Created Book class

---
+++ src/main/java/com/project/entities/Book.java
@@ -0,0 +1,136 @@
+package com.project.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Date;
+
+public class Book implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String author;
+	private String title;
+	private String description;
+	private String publishingCompany;
+	private Integer numberPages;
+	private String category;
+	private Date publicationDate;
+	private Instant registrationInstant;
+	
+	public Book() {
+		
+	}
+
+	public Book(Long id, String author, String title, String description, String publishingCompany, Integer numberPages,
+			String category, Date publicationDate, Instant registrationInstant) {
+		this.id = id;
+		this.author = author;
+		this.title = title;
+		this.description = description;
+		this.publishingCompany = publishingCompany;
+		this.numberPages = numberPages;
+		this.category = category;
+		this.publicationDate = publicationDate;
+		this.registrationInstant = registrationInstant;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPublishingCompany() {
+		return publishingCompany;
+	}
+
+	public void setPublishingCompany(String publishingCompany) {
+		this.publishingCompany = publishingCompany;
+	}
+
+	public Integer getNumberPages() {
+		return numberPages;
+	}
+
+	public void setNumberPages(Integer numberPages) {
+		this.numberPages = numberPages;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Date getPublicationDate() {
+		return publicationDate;
+	}
+
+	public void setPublicationDate(Date publicationDate) {
+		this.publicationDate = publicationDate;
+	}
+
+	public Instant getRegistrationInstant() {
+		return registrationInstant;
+	}
+
+	public void prePersist() {
+		registrationInstant = Instant.now();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+
+}
