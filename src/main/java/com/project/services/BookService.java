80de3af73c00e8b8a444090f3bffb5ef0d17a4c3
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-11-27 20:07:43 -0300

    Created delete method in the controller and service layers, alrealy with exception handling

--- src/main/java/com/project/services/BookService.java
+++ src/main/java/com/project/services/BookService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,6 +46,15 @@ public class BookService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}		
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 	
 	private void copyDtoToEntity(BookDTO dto, Book entity) {
